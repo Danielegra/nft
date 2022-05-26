42984a1fc4f09e044a8cc2e6cec2b9a438222e6b
Daniele Grassi <daniele.grassi@vidiemme.it>
2022-05-26 14:40:25 +0200

    :feature: setup tailwind

--- atoshland/src/views/HomeView.vue
+++ atoshland/src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">
+      Hello Atoshland ✌️🤟🤘
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import '../styles/app.css'
 
 @Options({
   components: {
